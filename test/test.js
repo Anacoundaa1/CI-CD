d858c711487a8e544dacf35bd6da27c117678e59
Anacoundaa1 <momo2706@hotmail.fr>
2023-12-20 11:10:33 +0100

    Test numéro 9

--- test/test.js
+++ test/test.js
@@ -1,22 +1,22 @@
 const addNumbers = require('../src/addition');
 
-describe('addNumbers function', () => {
-  it('adds two positive numbers', () => {
+describe('fonction addNumbers : ', () => {
+  it('Ajouter deux nombres positifs', () => {
     const result = addNumbers(2, 3);
     expect(result).toBe(5);
   });
 
-  it('adds a positive number and a negative number', () => {
+  it('Ajouter un nombre positif et un nombre negatif', () => {
     const result = addNumbers(5, -3);
     expect(result).toBe(2);
   });
 
-  it('adds two negative numbers', () => {
+  it('Ajouter deux nombres negatifs', () => {
     const result = addNumbers(-1, -7);
     expect(result).toBe(-8);
   });
 
-  it('adds zero and a number', () => {
+  it('Ajouter zero et un nombre', () => {
     const result = addNumbers(0, 9);
     expect(result).toBe(9);
   });
